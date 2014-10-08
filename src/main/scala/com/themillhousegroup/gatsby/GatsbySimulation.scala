287d28f1f31458b028e6eb73397602251d7fcf1a
John Marshall <john.marshall2@auspost.com.au>
2014-10-08 17:37:50 +1100

    Comments

--- src/main/scala/com/themillhousegroup/gatsby/GatsbySimulation.scala
+++ src/main/scala/com/themillhousegroup/gatsby/GatsbySimulation.scala
@@ -1,5 +1,12 @@
 package com.themillhousegroup.gatsby
 
-class GatsbySimulation {
+import io.gatling.core.scenario.Simulation
+
+/**
+ * A Gatling {@link Simulation} that automagically spins up
+ * "the thing at the back" so that we can actually test
+ * "the thing in the middle"
+ */
+class GatsbySimulation extends Simulation {
 
 }
