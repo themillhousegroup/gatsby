b1669c7957ab91f134a25a9a355c3d044c8383dc
John Marshall <john@themillhousegroup.com>
2014-10-12 07:52:07 +1100

    Trying multiple scenarios in one simulation

--- user-files/simulations/gatsbyexamples/DynamicGatsbySimulation.scala
+++ user-files/simulations/gatsbyexamples/DynamicGatsbySimulation.scala
@@ -13,12 +13,20 @@ class DynamicGatsbySimulation extends GatsbySimulation(9999) {
   val httpConf = http
     .baseURL("http://localhost:9999")
 
-  val scn = scenario("DynamicGatsbySimulation")
+  val scn1 = scenario("DynamicGatsbySimulation1")
     .exec(gatsbyHttp("request_1")(this)
-      .get("/foo/bar"))
+      .get("/first")
+      .check(status))
+    .pause(5)
+
+
+  val scn2 = scenario("DynamicGatsbySimulation2")
+    .exec(gatsbyHttp("request_2")(this)
+    .get("/second"))
     .pause(5)
 
   setUp(
-    scn.inject(atOnceUsers(1))
+    scn1.inject(atOnceUsers(1)),
+    scn2.inject(atOnceUsers(1))
   ).protocols(httpConf)
 }
