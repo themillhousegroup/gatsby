5846395f80bb23a6b83adbcc7cb420b04b72afd3
John Marshall <john@themillhousegroup.com>
2014-11-19 20:11:30 +1100

    Tests for mutual exclusion

---
+++ src/test/scala/com/themillhousegroup/gatsby/stubby/MutualExclusionSpec.scala
@@ -0,0 +1,67 @@
+package com.themillhousegroup.gatsby.stubby
+
+import org.specs2.mutable.Specification
+import org.specs2.mock.Mockito
+import org.apache.commons.lang3.time.StopWatch
+import scala.concurrent.{ Future, Await }
+import scala.concurrent.duration.Duration
+
+class MutualExclusionSpec extends Specification with Mockito {
+
+  val maxWait = Duration(5, "seconds")
+
+  class TestMutex extends EnforcesMutualExclusion with HasLogger {}
+
+  "EnforcesMutualExclusion trait" should {
+
+    "Allow direct access when there is no contention for the locked resource" in {
+
+      val t = new TestMutex()
+
+      val s = new StopWatch()
+      s.start
+
+      val fTime = t.acquireLock("direct").map { _ =>
+        s.stop
+        s.getTime
+      }
+
+      t.currentLockHolder must beSome("direct")
+
+      Await.result(fTime, maxWait) must beLessThan(900L)
+
+      t.releaseLock("direct")
+
+      t.currentLockHolder must beNone
+    }
+
+    "Force the second in line to wait" in {
+
+      val t = new TestMutex()
+
+      val firstLock = t.acquireLock("1")
+
+      Thread.sleep(10)
+      val secondLock = t.acquireLock("2")
+
+      Await.ready(firstLock, maxWait)
+
+      secondLock.isCompleted must beFalse
+
+      t.currentLockHolder must beSome("1")
+
+      t.releaseLock("1")
+
+      t.currentLockHolder must beNone
+
+      Await.ready(secondLock, maxWait)
+
+      secondLock.isCompleted must beTrue
+
+      t.releaseLock("2")
+
+      t.currentLockHolder must beNone
+
+    }
+  }
+}
