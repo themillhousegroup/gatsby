7e3b697f896fe1257678ed779d623dd464f99d8f
John Marshall <john.marshall2@auspost.com.au>
2014-11-25 11:42:04 +1100

    More test coverage

--- src/test/scala/com/themillhousegroup/gatsby/GatsbySimulationSpec.scala
+++ src/test/scala/com/themillhousegroup/gatsby/GatsbySimulationSpec.scala
@@ -1,7 +1,7 @@
 package com.themillhousegroup.gatsby
 
 import org.specs2.mutable.Specification
-import com.dividezero.stubby.core.model.StubExchange
+import com.dividezero.stubby.core.model.{ StubRequest, StubExchange }
 import org.specs2.mock.Mockito
 import com.themillhousegroup.gatsby.stubby.StubbyServer
 
@@ -17,7 +17,6 @@ class GatsbySimulationSpec extends Specification with Mockito {
   val mockExchange = mock[StubExchange]
 
   "GatsbySimulation" should {
-
     "Allow Simulation-wide exchanges to be defined" in {
       val testGatsbySimulation = new TestGatsbySimulation(Seq(mockExchange))
 
@@ -27,5 +26,38 @@ class GatsbySimulationSpec extends Specification with Mockito {
       //      there was one(mockStubbyServer).addExchange(mockExchange)
 
     }
+
+    "Allow exchanges to be added per-request" in {
+      val testGatsbySimulation = new TestGatsbySimulation(Seq(mockExchange))
+
+      val mockStubExchange = givenStubExchange("GET", "/bar")
+
+      testGatsbySimulation.addExchange("foo", mockStubExchange) must beTrue
+
+      testGatsbySimulation.addExchange("foo", mockStubExchange) must beFalse
+    }
+
+    "Allow exchanges to be removed per-prefix" in {
+      val testGatsbySimulation = new TestGatsbySimulation(Seq(mockExchange))
+
+      val mockStubExchange = givenStubExchange("GET", "/bar")
+
+      testGatsbySimulation.addExchange("foo", mockStubExchange) must beTrue
+
+      testGatsbySimulation.removeExchange("fo") must beTrue
+
+      testGatsbySimulation.removeExchange("fox") must beFalse
+
+      testGatsbySimulation.removeExchange("fo") must beFalse
+    }
+  }
+
+  def givenStubExchange(method: String, path: String) = {
+    val mockStubExchange = mock[StubExchange]
+    val mockStubRequest = mock[StubRequest]
+    mockStubExchange.request returns mockStubRequest
+    mockStubRequest.method returns Some(method)
+    mockStubRequest.path returns Some(path)
+    mockStubExchange
   }
 }
