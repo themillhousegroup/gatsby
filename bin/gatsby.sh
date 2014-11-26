047af9e23dfbc8a37b6c0eaa8d4642f4249547b1
John Marshall <john.marshall2@auspost.com.au>
2014-11-26 12:32:25 +1100

    Make the bash script runnable from anywhere, so you can version your user-files elsewhere

--- bin/gatsby.sh
+++ bin/gatsby.sh
@@ -5,18 +5,23 @@
 
 # This is useful when developing new Gatsby features and wanting to test them out with real user-tests.
 
-# You should invoke this from the project root, i.e. ./bin/gatsby.sh
-
 # To run non-interactively (i.e. skip prompting of the simulation to be run) use:
 
-# ./bin/gatsby.sh [-f] -s <simulation.class>
+# <path-to-bin-dir>/gatsby.sh [-f] -s <simulation.class>
 
-# e.g. ./bin/gatsby.sh -f -s gatsbyexamples.BasicGatsbySimulation
+# For example, if invoking from the Gatsby project root: 
+# ./bin/gatsby.sh -f -s gatsbyexamples.BasicGatsbySimulation
 
+INITIAL_DIR=`pwd`
+BIN_DIR=$(dirname $0)
+cd $BIN_DIR
+cd ..
+PROJECT_ROOT=`pwd`
 
 if [ $# -gt 0 ] && [ "$1" == "-f" ]
 then
   echo "Forcing an SBT stage..."
+  cd $PROJECT_ROOT
   sbt stage
   shift
   SBT_STAGE_RESULT=$?
@@ -24,8 +29,10 @@ else
   SBT_STAGE_RESULT=0
 fi
 
+cd $INITIAL_DIR
+
 if [ $SBT_STAGE_RESULT -eq 0 ]
 then
   echo "Now launching Gatling..."
-  ./bin/gatling.sh "$@"
+  $BIN_DIR/gatling.sh "$@"
 fi
